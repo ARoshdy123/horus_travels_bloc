f51031ca3c7c4986d3d97bec0ec8fdd4ac54e103
Dell <marina.magdy.ibrahim97@gmail.com>
2025-01-25 20:34:16 +0200

    + add bloc to login

---
+++ lib/authentication/screens/widgets/login_widgets/login_cubit.dart
@@ -0,0 +1,26 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:meta/meta.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+part 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  LoginCubit() : super(LoginInitial());
+   // Validates user credentials
+   validateUser(String email, String password) async {
+    try {
+      emit(LoginLoading());
+       final SharedPreferences prefs = await SharedPreferences.getInstance();
+       final storedEmail = prefs.getString('email');
+       final storedPassword = prefs.getString('password');
+       if (email == storedEmail && password == storedPassword){
+         emit(LoginAuthenticated());
+       }else{
+         emit(LoginUnAuthenticated());
+       }
+       return ;
+   }catch(e){
+      emit(LoginError(e.toString()));
+    }
+}}
---
+++ lib/authentication/screens/widgets/main_widgets/custom_dialog.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CustomDialog extends StatelessWidget {
+   CustomDialog({super.key,required this.text});
+   final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      content: Text(text),
+      actions: [
+        TextButton(onPressed: (){
+          Navigator.pop(context);
+        }, child:const Text("Close") )
+      ],
+    );
+  }
+}
