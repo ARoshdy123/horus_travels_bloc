ad64fbb45dd31e53b3df912878864208b6d705fa
Ahmed Roshdy Ragheb Mohamed <ahmedroshdy6555554@gmail.com>
2025-01-25 22:19:58 +0200

    Final fixes on screens

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'authentication/screens/widgets/login_widgets/login_cubit.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
